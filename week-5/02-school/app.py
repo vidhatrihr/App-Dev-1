0c7a68a062af3f3661fc3e14299e27bb62f8bcdf
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2024-10-31 15:30:19 +0530

    02-school sqlite3

--- week-5/02-school/app.py
+++ week-5/02-school/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, render_template, redirect
-from models import *
+import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect('test.db', check_same_thread=False)
+cur = conn.cursor()
 
 
 @app.route('/')
 def index():
+  cur.execute('select * from students')
+  students = cur.fetchall()
+
+  cur.execute('select * from courses')
+  courses = cur.fetchall()
+
+  cur.execute('select * from enrolls')
+  enrolls = cur.fetchall()
   return render_template('index.html', students=students, courses=courses, enrolls=enrolls)
 
 
@@ -15,8 +25,8 @@ def add_student_route():
     name = request.form['name']
     place = request.form['place']
     age = int(request.form['age'])
-    student = Student(name, place, age)
-    add_student(student)
+    cur.execute("INSERT INTO students (name, place, age) VALUES (?,?,?)", (name, place, age))
+    conn.commit()
     return redirect('/')
   elif request.method == 'GET':
     return render_template('add-student.html')
@@ -27,8 +37,8 @@ def add_course_route():
   if request.method == 'POST':
     name = request.form['name']
     credits = int(request.form['credits'])
-    course = Course(name, credits)
-    add_course(course)
+    cur.execute('INSERT INTO courses (name, credits) VALUES (?,?)', (name, credits))
+    conn.commit()
     return redirect('/')
   elif request.method == 'GET':
     return render_template('add-course.html')
@@ -41,8 +51,9 @@ def add_enroll_route():
     course_id = int(request.form['course_id'])
     term = request.form['term']
     year = int(request.form['year'])
-    enroll = Enroll(student_id, course_id, term, year)
-    add_enroll(enroll)
+    cur.execute('INSERT INTO enrolls (student_id, course_id, term, year) VALUES (?,?,?,?)',
+                (student_id, course_id, term, year))
+    conn.commit()
     return redirect('/')
   elif request.method == 'GET':
     return render_template('add-enroll.html')
